10b60ecbae950e5de62ff21f4d3501849960c357
isauracg <isaura.carrgal@gmail.com>
2023-04-16 03:10:04 -0400

    Fin del ejercicio Cajero Automatico

--- _M2_S8_Drilling_Cajero automático/M2_S8_IsauraCarrascoGaldames_Drilling/assets/js/script.js
+++ _M2_S8_Drilling_Cajero automático/M2_S8_IsauraCarrascoGaldames_Drilling/assets/js/script.js
@@ -1,10 +1,17 @@
-/* Cada uno de los clientes de nuestro banco contará con un nombre, un identificador, una clave y un
-saldo en su cuenta. */
+/*  Realizaremos un cajero automático en el cual el usuario deberá validar
+    su identidad. Una vez que se valide la identidad el usuario podrá ver
+    su saldo, realizar giros o depósitos. 
+*/
 
+// --------------------------------
+/* REQUERIMIENTO :  
+Cada uno de los clientes de nuestro banco contará con un nombre, un identificador (tipo rut), 
+una clave y un saldo en su cuenta.
+*/
+
+// Se define la clase y el constructor de objetos tipo 'Cliente' para simular una BD que conetenga los datos de los clientes */
 class Cliente {
-  // creacion de un constructor para la creación de objetos de tipo cliente
   constructor(nombre, rut, clave, saldo) {
-    //declara la incialización de variables
     this.nombre = nombre;
     this.rut = rut;
     this.clave = clave;
@@ -12,40 +19,166 @@ class Cliente {
   }
 }
 
-// creación de los clientes usando el constructor de 'Cliente'
-var cliente1 = new Cliente("Camila Fernandez", "12345678-2", "Pass123#", 30000);
-var cliente2 = new Cliente("Fernanda Marín", "12345678-0", "Pass456#", 50000);
-var cliente3 = new Cliente("Tito Astudillo", "12345678-1", "Pass789#", 60000);
+// Se crean los objetos tipo 'Cliente' con sus valores correspondientes
+var cliente1 = new Cliente("Fulanito", "27080900-1", "pass1", 30000);
+var cliente2 = new Cliente("Juan", "27080900-2", "pass2", 40000);
+var cliente3 = new Cliente("Maria", "27080900-3", "pass3", 50000);
 
-// Lista de tamaño 3 con 3 elementos
-var clientes = [cliente1, cliente2, cliente3 ];
-console.log(clientes);
+// Se crea un arreglo que contiene a todos los clientes anteriormente creados
+var listaClientes = [cliente1, cliente2, cliente3];
 
+// --------------------------------
 // MENSAJE DE BIENVENIDA //
+alert("Bienvenido/a a CITYBANK");
 
-/* alert ("Hola, Bienvenido/a a CITYBANK."); */
+// Se declaran e inicializan las variables que solicitan, mediante la función prompt,
+// los datos 'rut' y 'clave' al usuario , y los almacenan como strings
+var rutCliente = prompt("Ingrese su rut");
+var claveCliente = prompt("Ingrese su clave");
 
-var rutCliente = prompt ("ingrese rut");
-var claveCliente = prompt ("ingrese clave");
+// variable booleana que indica el estado del usuario autorizado si es true, denegado si es false
+var permiso = false;
+// variable que almacena al cliente validado
+var cliente;
 
-var valido = clientes.indexOf(rutCliente,claveCliente); 
+// --------------------------------
+// VALIDACIÓN DEL USUARIO EXISTENTE //
+// --------------------------------
+/* REQUERIMIENTO: 
+Cuando se ingrese el identificador y la clave, se revisará si coincide con alguno de los clientes
+registrados. Si no coincide, se mostrará un mensaje de error.
+ */
 
-console.log(valido);
+for (let indice = 0; indice < listaClientes.length; indice++) {
+  //Se recorre el arreglo con ciclo FOR
 
-/* if (valido == -1) {
-    console.log ("Rut o Clave inválidos");
-    alert ("Rut o Clave inválidos\nPor favor ingrese sus datos nuevamente.");
-    rutCliente = prompt ("ingrese rut");
-    claveCliente = prompt ("ingrese clave");
+  //Se establece la condicional IF para validar si los datos coinciden o no con los de 'listaClientes'
+  if (
+    listaClientes[indice].rut == rutCliente &&
+    listaClientes[indice].clave == claveCliente
+  ) {
+    // Si los datos coinciden:
+    permiso = true; // El estado de permiso cambia a true
+    console.log("Bienvenido Cliente"); //Se imprime mensaje de bienvenida en consola
+    alert("Hola, bienvenido " + listaClientes[indice].nombre); // Se emite alerta de Bienvenida con nombre del usuario
+    cliente = listaClientes[indice]; //Se almacenan los datos del cliente en var 'cliente'
+    menu(cliente); //Se accede al menú de opciones
+    break; //Se detiene el ciclo FOR
+  } else {
+    //Si los datos no coinciden:
+    permiso = false; // Se mantiene el permiso en estado false
 
-} else if (valido != -1){
-    alert ("Bienvenido");
-} else {
-    alert ("ERROR");
-} */
+    //Se Ejecuta ciclo DO WHILE mientras los datos no coincidan, para solicitar nuevamente los datos
+    do {
+      console.log("Datos inválidos"); //Se imprime en consola mensaje de Error
+      alert("Datos inválidos."); // Se alerta al usuario sobre el Error
+      rutCliente = prompt("Ingrese nuevamente su rut"); // Se solicita 'rut' nuevamente
+      claveCliente = prompt("Ingrese nuevamente su clave"); // Se solicita 'clave' nuevamente
+    } while (
+      listaClientes[indice].rut != rutCliente &&
+      listaClientes[indice].clave != claveCliente
+    );
 
+    // Al ingresar datos coincidentes, se termina el ciclo anterior y:
+    permiso = true; // El estado de permiso cambia a true
+    console.log("Bienvenido Cliente"); //Se imprime mensaje de bienvenida en consola
+    alert("Hola, bienvenido " + listaClientes[indice].nombre); // Se emite alerta de Bienvenida con nombre del usuario
+    cliente = listaClientes[indice]; //Se almacenan los datos del cliente en var 'cliente'
+    menu(cliente); //Se accede al menú de opciones
+    break; //Se detiene el ciclo FOR
+  }
+}
 
+// --------------------------------
+/* REQUERIMIENTO:
+Se muestra el menú que se repite hasta que el usuario selecciona Salir. 
+Si ingresa un número distinto se envía un mensaje de error y se repite.
+Si realiza giros o depósitos, su saldo se modificará.
+ */
 
+function menu(cliente) {
+  let op = ""; //Se declara la variable 'op' para referirse a la opción del usuario
 
+  do {
+    // Ciclo DO WHILE que se ejecuta siempre que 'op' sea distinto de 4 (salir)
 
+    op = prompt(
+      //A la var 'op' se le asigna el valor solicitado mediante funcion prompt al usuario
+      "Seleccione que desea hacer\n" +
+        "1.- Ver saldo\n" +
+        "2.- Realizar giro\n" +
+        "3.- Realizar deposito\n" +
+        "4.- Salir"
+    );
 
+    switch (
+      op // mediante un SWITCH CASE vamos ejecutando las funciones para cada opción
+    ) {
+      case "1": //ver saldo
+        //verSaldo()
+        alert("Su saldo actual es: CLP$ " + cliente.saldo);
+        break;
+
+      /* ---------------------------------------------------------------- */
+      /* CASE 2 - GIRAR */
+      case "2": //realizar giro
+        //realizarGiro()
+        var montoGiro = parseInt(
+          prompt(
+            "Su saldo actual es: CLP$ " +
+              cliente.saldo +
+              "\n\nIngrese el monto que desea girar:"
+          )
+        );
+        var saldoAntesGiro = parseInt(cliente.saldo);
+
+        if (saldoAntesGiro >= montoGiro) {
+          var saldoPosGiro = saldoAntesGiro - montoGiro;
+          console.log(saldoPosGiro);
+          alert("Tu nuevo saldo es: " + saldoPosGiro);
+          cliente.saldo = saldoPosGiro;
+        } else {
+          console.log("Saldo insufuciente.");
+          alert("Tus saldo es insufciente\npara realizar esta operación."); //salir
+        }
+        break;
+
+      /* ---------------------------------------------------------------- */
+      /* CASE 3 - DEPOSITAR */
+      case "3": //realizar deposito
+        var montoDepost = parseInt(
+          prompt(
+            "Su saldo actual es: CLP$ " +
+              cliente.saldo +
+              "\n\nIngrese el monto que desea depositar:"
+          )
+        );
+        var saldoAntesDepost = parseInt(cliente.saldo);
+
+        if (montoDepost > 0) {
+          var saldoPosDepost = saldoAntesDepost + montoDepost;
+          console.log(saldoPosDepost);
+          alert("Tu nuevo saldo es: " + saldoPosDepost);
+          cliente.saldo = saldoPosDepost;
+        } else {
+          console.log("Monto es 0 o menor");
+          alert("No puedes hacer un depósito por CLP$ 0 ,\npor favor ingresa un monto superior a CLP$ 0."); //salir
+        }
+        break;
+
+      /* ---------------------------------------------------------------- */
+      /* CASE 4 - SALIR */
+      case "4": // Si 'op' es "4", se imprime mensaje en consola y se muestra alerta con mensaje de despedida para el usuario
+        console.log("Finalizado por usuario");
+        alert("GRACIAS por su visita, adiós."); //salir
+        break;
+      default: //no se cumple ningun caso, mensaje de ERRor por dafault
+        console.log("Opción no válida");
+        alert("ERROR\n\nIngresa una opción válida, por favor.");
+    }
+  } while (op != "4"); //Mientras 'op' sea diferente de 4
+}
+
+function girar(cliente, montoGiro) {
+  console.log(montoGiro);
+}
